cf49c0a756516cfdb6f59848bac1e292717b4ada
Harshvardhan Patil <harshvardhanpatil@Harshvardhans-MacBook-Air.local>
2023-11-04 21:23:04 +0000

    mapping added to images

--- frontend/src/app/Components/Lander.js
+++ frontend/src/app/Components/Lander.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Loading from "./Loading";
+import imagemapping from '../Mappings/imagemapping.json'
 
 export default function Lander() {
   const [teams, setTeams] = useState(null);
@@ -56,10 +57,14 @@ export default function Lander() {
     <div>
       {fixtureInfo && Array.isArray(fixtureInfo) ? (
         fixtureInfo.map((fixture) => {
+          const team1ImageMap = imagemapping.find(d =>  d.id == fixture.teams[0].team.id).imageId ;
+          const team2ImageMap = imagemapping.find(d =>  d.id == fixture.teams[1].team.id).imageId ;
           return (
             <div id={fixture.id}>
+              <img src={`https://resources.premierleague.com/premierleague/badges/50/t${team1ImageMap}@x2.png`}/>
               <span>{fixture.teams[0].team.name}</span>
               {" VS "}
+              <img src={`https://resources.premierleague.com/premierleague/badges/50/t${team2ImageMap}@x2.png`}/>
               <span>{fixture.teams[1].team.name}</span>
               <span>{new Date(fixture.kickoff.millis).toUTCString()}</span>
             </div>
