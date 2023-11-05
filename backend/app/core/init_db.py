aea0af21f1a32e5b74804c230e7edab1a0a96465
mradzikowski <radzik.radzikowski18@gmail.com>
2023-11-05 07:39:31 +0000

    Fix init core

--- backend/app/core/init_db.py
+++ backend/app/core/init_db.py
@@ -20,26 +20,26 @@ async def init_database_on_startup():
             # Iterate over the CSV rows
             for row in csv_reader:
                 # Assuming the CSV columns match the columns of the table
-                date = row[1].split("/")
-                date = date[2] + "-" + date[1] + "-" + date[0]
+                # date = row[1].split("/")
+                # date = date[2] + "-" + date[1] + "-" + date[0]
 
                 does_data_exist = await Fixture.check_if_the_fixture_exists(
                     db_session=session,
                     first_team=row[2],
                     second_team=row[3],
-                    date=date,
+                    date=row[1],
                 )
                 if not does_data_exist:
                     data = Fixture(
                         home_team=row[2],
                         away_team=row[3],
-                        date=date,
+                        date=row[1],
                         full_time_home_goals=int(row[4]),
                         full_time_away_goals=int(row[5]),
                     )
                     session.add(data)
                 else:
                     logger.info(
-                        f"Data already exists for {row[2]} vs {row[3]} on {date}. Duplicate data not added.",
+                        f"Data already exists for {row[2]} vs {row[3]} on {row[2]}. Duplicate data not added.",
                     )
         await session.commit()
